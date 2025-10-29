f25c45f0d5257cee40d4fdf6c73ab9459ef538ae
kei077 <kawtartaik123@gmail.com>
2025-10-29 16:42:43 +0100

    Remove public IP mapping on launch from public subnets

--- infra/networking.tf
+++ infra/networking.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
   tags = {
     Name = "${var.project_name}-public-a"
     Tier = "public"
@@ -30,7 +30,7 @@ resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
   tags = {
     Name = "${var.project_name}-public-b"
     Tier = "public"
