a2dc3142d5612ea5fed32128d8d556727aaf0c9a
kei077 <kawtartaik123@gmail.com>
2025-11-05 20:52:25 +0100

    Add IAM role for RDS enhanced monitoring

--- infra/iam.tf
+++ infra/iam.tf
@@ -64,3 +64,27 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.project_name}-ec2-instance-profile"
   role = aws_iam_role.ec2_role.name
 }
+
+# IAM role for RDS Enhanced Monitoring
+resource "aws_iam_role" "rds_monitoring_role" {
+  name = "${var.project_name}-rds-monitoring-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "monitoring.rds.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attaching the policy to a specific role
+resource "aws_iam_role_policy_attachment" "rds_monitoring_attach" {
+  role       = aws_iam_role.rds_monitoring_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+}
